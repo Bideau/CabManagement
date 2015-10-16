9303038d6d0a54a9b4ec9508261ed5c7fe5610d6
Adullan <arnaud.bes66@hotmail>
2015-10-16 10:31:45 +0200

    ADD Package hud avec painter et Artist

---
+++ Dev/JavaInterface/CarsInterface/src/hud/StreetDrawer.java
@@ -0,0 +1,62 @@
+package hud;
+
+import java.awt.BasicStroke;
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import structures.Area;
+import structures.Street;
+
+public class StreetDrawer {
+
+	Area area;
+	
+	private double VertexHeight = 0.0;
+	private double VertexWidth = 0.0;
+	
+	public StreetDrawer(){
+		this.area = new Area();
+	}
+	
+	public StreetDrawer(Area _area, double _vertexWidth, double _vertexHeight){
+		this.area = _area;
+		this.VertexHeight = _vertexHeight;
+		this.VertexWidth = _vertexWidth;
+	}
+
+	public void paintStreets(Graphics g){
+
+		//************* Get and draw Streets ***************//
+		for(Street object : this.area.getStreetsList()){
+
+			double origineX = object.getFirstVertice().getCoord().getX();
+			double origineY = object.getFirstVertice().getCoord().getY();;
+			double destX = object.getSecondVertice().getCoord().getX();
+			double destY = object.getSecondVertice().getCoord().getY();;
+
+			origineX = origineX + (VertexWidth/2);
+			origineY = origineY + (VertexHeight/2);
+
+			destX = destX + (VertexWidth/2);
+			destY = destY + (VertexHeight/2);
+
+			// LINE
+			// x Origine  -  y Origine
+			// x Dest     -  y Dest
+			//g.drawLine(15, 20, 500, 20);
+
+			// Boost thickness
+			((Graphics2D) g).setStroke(new BasicStroke(6));
+			g.setColor(new Color(Integer.parseInt("0000FF",16)));
+
+			// Draw Street
+			g.drawLine((int)Math.round(origineX),
+					(int)Math.round(origineY),
+					(int)Math.round(destX),
+					(int)Math.round(destY));
+		}
+
+	}
+
+}
---
+++ Dev/JavaInterface/CarsInterface/src/hud/VertexDrawer.java
@@ -0,0 +1,52 @@
+package hud;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.geom.Rectangle2D;
+
+import structures.Area;
+import structures.Vertex;
+
+public class VertexDrawer {
+
+	private Area area;
+	
+	private double x = 0.0;
+	private double y = 0.0;
+	
+	private double VertexHeight = 0.0;
+	private double VertexWidth = 0.0;
+
+	public VertexDrawer(){
+		area = new Area();
+	}
+
+	public VertexDrawer(Area _area, double _vertexWidth, double _vertexHeight){
+		this.area = _area;
+		this.VertexHeight = _vertexHeight;
+		this.VertexWidth = _vertexWidth;
+	}
+
+	public void paintVertices(Graphics2D g2d){
+
+		//************* Get and draw Vertices **************//
+		for(Vertex object : this.area.getVerticesList()){
+			x = object.getCoord().getX();
+			y = object.getCoord().getY();
+
+			System.out.println("X : " + x + "\nY : " + y + "\nName : " + object.getName());
+
+			// RECTANGLE		10.0,10.0,20.0,20.0
+			// x - y
+			// Longueur - Hauteur
+			Rectangle2D cell = new Rectangle2D.Double(x,y,VertexWidth,VertexHeight);
+			Rectangle2D cell2 = new Rectangle2D.Double(x,y,VertexWidth,VertexHeight);
+
+			g2d.setColor(new Color(Integer.parseInt("000000",16)));
+			g2d.fill(cell);
+			g2d.setColor(new Color(Integer.parseInt("FF0000",16)));
+			g2d.draw(cell2);
+		}
+	}
+}
+
