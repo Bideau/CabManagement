9ff708a42f471712cc7efc131462b850aa1f8a90
Bideau <guilhem.bideau@gmail.com>
2015-10-16 14:50:08 +0200

    restructuration of java package

---
+++ Dev/JavaInterface/CarsInterface/src/structures/map/Carte.java
@@ -0,0 +1,57 @@
+package structures.map;
+
+import java.util.ArrayList;
+
+public class Carte {
+
+	private ArrayList<Vertex> verticesList;
+	private ArrayList<Street> streetsList;
+	private ArrayList<Bridge> bridgesList;
+	private MapSize weight;
+
+	
+	public Carte() {
+		this.verticesList = new ArrayList<Vertex>();
+		this.streetsList = new ArrayList<Street>();
+		this.bridgesList = new ArrayList<Bridge>();
+		this.weight=new MapSize();
+	}
+
+	public ArrayList<Vertex> getVertices() {
+		return verticesList;
+	}
+
+	public void setVertices(ArrayList<Vertex> verticesList) {
+		this.verticesList = verticesList;
+	}
+
+	public ArrayList<Street> getStreets() {
+		return streetsList;
+	}
+
+	public void setStreets(ArrayList<Street> streetsList) {
+		this.streetsList = streetsList;
+	}
+
+	public ArrayList<Bridge> getBridges() {
+		return bridgesList;
+	}
+
+	public void setBridges(ArrayList<Bridge> bridgesList) {
+		this.bridgesList = bridgesList;
+	}
+
+	public MapSize getWeight() {
+		return weight;
+	}
+
+	public void setWeight(MapSize weight) {
+		this.weight = weight;
+	}
+
+	public String toString() {
+		return "Carte [verticesList=" + verticesList + ", streetsList="
+				+ streetsList + ", bridgesList=" + bridgesList + ", weight="
+				+ weight + "]";
+	}	
+}
---
+++ Dev/JavaInterface/CarsInterface/src/structures/taxi/CabInfo.java
@@ -0,0 +1,14 @@
+package structures.taxi;
+
+public class CabInfo {
+	
+	private int odometer;
+	private String destination;
+	private LocVertex locNow;
+	private LocVertex locNext;
+
+	public CabInfo() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
