638e7c151ecb06fd024eaad0cbfcdfcfeae317b7
Bideau <guilhem.bideau@gmail.com>
2015-10-20 18:05:38 +0200

    MaJ communication server

--- Dev/PythonServer/simplesocket.py
+++ Dev/PythonServer/simplesocket.py
@@ -1,27 +1,40 @@
 import signal, sys, ssl
 from SimpleWebSocketServer import WebSocket, SimpleWebSocketServer, SimpleSSLWebSocketServer 
 from optparse import OptionParser
-
-
+from sys import argv
+import json
 
 clients = []
+carte = []
 class SimpleChat(WebSocket):
 
     def myfonction(self):
-        print 'hello toto'
+        print 'Someone is connected'
 
+    #Function when someone send a message
     def handleMessage(self):
         print self.data
+        if self.data == 'Initialisation':
+            fic = open('/media/guinux/Data/Cours/Actuel/IntMobile/CabManagement/Dev/JavaInterface/json/test.json')
+            message = fic.read()
+            tmpJson = json.loads(message)
+            tmp = json.dumps(tmpJson)
+            clients[-1].sendMessage(unicode(tmp))
         for client in clients:
-            if client != self:
-                client.sendMessage(self.address[0] + ' - ' + self.data)
+             client.sendMessage(self.address[0] + ' - ' + self.data)
+
+    #Function when someone connect
     def handleConnected(self):
         print self.address, 'connected'
        # for client in clients:
-        #    client.sendMessage(self.address[0] + u' - connected')
+       #     client.sendMessage(self.address[0] + u' - connected')
+          #  client.sendMessage(unicode(tmpJson)) 
         self.myfonction()
         clients.append(self)
-        clients[-1].sendMessage(u'your are connected')
+        clients[-1].sendMessage(u'you are connected!')
+        
+
+    #Function when someone disconnect
     def handleClose(self):
         clients.remove(self)
         print self.address, 'closed'
@@ -29,4 +42,5 @@ class SimpleChat(WebSocket):
             client.sendMessage(self.address[0] + u' - disconnected')
 
 server = SimpleWebSocketServer('0.0.0.0', 8000, SimpleChat)
+
 server.serveforever()
