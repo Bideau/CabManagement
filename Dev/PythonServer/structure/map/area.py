aebb82aa3d209def2c0c095fc5eb9fb2698e8d0b
Bideau <guilhem.bideau@gmail.com>
2015-10-20 18:03:03 +0200

    Revision strucutre map

--- Dev/PythonServer/structure/map/area.py
+++ Dev/PythonServer/structure/map/area.py
@@ -1,3 +1,11 @@
+from map import Map
 class Area:
     #Area object
-    
+    def __init__(self):
+        self.name = ''
+        self.map = Map()
+    def parsing(self,value):
+        if 'name' in value:   
+            self.name = value['name']
+        if 'map' in value:   
+            self.map.parsing(value['map'])
--- Dev/PythonServer/structure/map/bridge.py
+++ Dev/PythonServer/structure/map/bridge.py
@@ -1,5 +1,13 @@
+from dest import Dest
 class Bridge:
-    from = ''
-    weight = 0.0
-    to = Dest()
-
+    def __init__(self):
+        self.src = ''
+        self.weight = 0.0
+        self.to = Dest()
+    def parsing(self,value):
+        if 'src' in value:
+            self.src = value['src']
+        if 'weight' in value:
+            self.weight = value['weight']
+        if 'to' in value:
+            self.to.parsing(value['to'])
