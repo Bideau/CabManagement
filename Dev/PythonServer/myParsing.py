1bff8929c4956caae894a1a3080c033970021df2
Bideau <guilhem.bideau@gmail.com>
2015-10-21 14:22:16 +0200

    Update of the websocket

--- Dev/PythonServer/myParsing.py
+++ Dev/PythonServer/myParsing.py
@@ -1,42 +1,59 @@
 import json
 from structure.map import Area
+from structure.cab import CabRequest
 from structure.cab import CabInfo
+from structure.cab import CabStatus
+from traitementCab import TraitementCab
 class myParsing:
     
     def __init__(self):
         self.carte=[]
-        self.pathMap = ''
-        self.pathCab = ''
+        self.process=TraitementCab()
+        self.cabList=[]
+        self.myFile=''
 
+    def readFile(self,path):
+        self.myFile = open(path)
+        myJSON = self.myFile.read()
+        return myJSON
+        
 
     # Parsing function of the map
-    def parsingArea (self,pathMap):
-        self.pathMap = pathMap
-        myFile = open(self.pathMap)
-        myJSON = myFile.read()
+    def parsingArea (self,myJSON):
         json_data = json.loads(myJSON)
         if 'areas' in json_data:
             for i in json_data['areas']:
                 tmpArea = Area()
-                #tmpArea.name = i['name']
                 tmpArea.parsing(i)
                 self.carte.append(tmpArea)
-            print self.carte[0]
-        myFile.close()
+               # print tmpArea
+        self.myFile.close()
 
     # Parsing function of the cab
-    def parsingCab(self,pathCab):
-        self.pathCab = pathCab
-        myFile = open(self.pathCab)
-        myJSON = myFile.read()
-        print myJSON
+    def parsingCabInfo(self,myJSON):
         json_data = json.loads(myJSON)
-        tmpCab = CabInfo()
+        tmpCab = CabStatus()
+        test = CabInfo()
+        test.parsing(json_data)
+        tmpCab.cabinfo=test
+        self.cabList.append(tmpCab)
+        self.myFile.close()
+
+    # Parsing function of a cab request
+    def parsingCabReq(self,myJSON):
+        json_data = json.loads(myJSON)
+        tmpCab = CabRequest()
         tmpCab.parsing(json_data)
-        myFile.close()
-            
+        # Adding to the cab request list
+        self.process.request(tmpCab)
+        self.process.calcPath(self.carte,self.cabList[0])
+        self.myFile.close()
+
+    def parsingRepArdui(self,myJSON):
+        json_data = json.loads(myJSON)
 
 test = myParsing()
-test.parsingArea('/media/guinux/Data/Cours/Actuel/IntMobile/CabManagement/Dev/JavaInterface/json/Initialize.json')
-test.parsingCab('/media/guinux/Data/Cours/Actuel/IntMobile/CabManagement/Dev/JavaInterface/json/taxi.json')
+test.parsingArea(test.readFile('/media/guinux/Data/Cours/Actuel/IntMobile/CabManagement/Dev/JavaInterface/json/test.json'))
+test.parsingCabInfo(test.readFile('/media/guinux/Data/Cours/Actuel/IntMobile/CabManagement/Dev/JavaInterface/json/taxi2.json'))
+test.parsingCabReq(test.readFile('/media/guinux/Data/Cours/Actuel/IntMobile/CabManagement/Dev/JavaInterface/json/request2.json'))
 
--- Dev/PythonServer/simplesocket.py
+++ Dev/PythonServer/simplesocket.py
@@ -3,6 +3,7 @@ from SimpleWebSocketServer import WebSocket, SimpleWebSocketServer, SimpleSSLWeb
 from optparse import OptionParser
 from sys import argv
 import json
+import time
 
 clients = []
 carte = []
@@ -15,14 +16,31 @@ class SimpleChat(WebSocket):
     def handleMessage(self):
         print self.data
         if self.data == 'Initialisation':
+            print 'init'
+
             fic = open('/media/guinux/Data/Cours/Actuel/IntMobile/CabManagement/Dev/JavaInterface/json/test.json')
             message = fic.read()
             tmpJson = json.loads(message)
             tmp = json.dumps(tmpJson)
+            fic.close()
             clients[-1].sendMessage(unicode(tmp))
-        for client in clients:
-             client.sendMessage(self.address[0] + ' - ' + self.data)
-
+            print 'init1'
+            fic = open('/media/guinux/Data/Cours/Actuel/IntMobile/CabManagement/Dev/JavaInterface/json/taxi.json')
+            message = fic.read()
+            tmpJson = json.loads(message)
+            tmp = json.dumps(tmpJson)
+            fic.close()
+            clients[-1].sendMessage(unicode(tmp))
+            print 'init2'
+            time.sleep(2)
+            fic = open('/media/guinux/Data/Cours/Actuel/IntMobile/CabManagement/Dev/JavaInterface/json/taxi3.json')
+            print 'init3'
+            message = fic.read()
+            tmpJson = json.loads(message)
+            tmp = json.dumps(tmpJson)
+            fic.close()
+            clients[-1].sendMessage(unicode(tmp))
+            print 'init4'
     #Function when someone connect
     def handleConnected(self):
         print self.address, 'connected'
